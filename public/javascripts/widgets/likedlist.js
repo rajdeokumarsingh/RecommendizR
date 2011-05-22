bc22955050d2443559b6688b170c023d8b89292d
Jean-Baptiste Lemée <jblemee@yahoo.fr>
2011-05-22 14:25:40 +0200

    Add refresh button on list

--- public/javascripts/widgets/likedlist.js
+++ public/javascripts/widgets/likedlist.js
@@ -16,7 +16,6 @@ require.def("widgets/likedlist", ["jquery", "utils", "widgets/twostatesbutton"],
             });
          }
 
-
          var onSuccess = function(containerId, isLikedResource, switchLikeResource, switchIgnoreResource, likedList) {
             if (likedList.length == 0) {
                $('#' + containerId).html('<p>No data.</p>');
@@ -30,10 +29,9 @@ require.def("widgets/likedlist", ["jquery", "utils", "widgets/twostatesbutton"],
                   var ahref = $('<a>').addClass('histolink').attr('href', '#!/liked/' + el.id).attr('alt', el.description).text(el.name);
                   var li = $('<li>').attr('id', containerId + '-li-' + el.id);
                   $(li).append(ahref).append(likeOrUnlikeButton).append(ignoreButton);
-                  //$(ahref).click(self.onClickLiked.execute.curry({limit:10, likedId:el.id}));
                   $('#' + containerId + '-list').append(li);
-                  TwoStatesButton.Instance(containerId + "-list-a-"+ el.id, 'unlike', 'like', switchLikeResource, {likedId:el.id}, 'liked', el.liked, 'likebutton'+el.id);
-                  TwoStatesButton.Instance(containerId + "-list-ignore-"+ el.id, 'unignore', 'ignore', switchIgnoreResource, {likedId:el.id}, 'ignored', el.ignored, 'ignorebutton'+el.id);
+                  TwoStatesButton.Instance(containerId + "-list-a-" + el.id, 'unlike', 'like', switchLikeResource, {likedId:el.id}, 'liked', el.liked, 'likebutton' + el.id);
+                  TwoStatesButton.Instance(containerId + "-list-ignore-" + el.id, 'unignore', 'ignore', switchIgnoreResource, {likedId:el.id}, 'ignored', el.ignored, 'ignorebutton' + el.id);
                });
 
             }
@@ -44,6 +42,11 @@ require.def("widgets/likedlist", ["jquery", "utils", "widgets/twostatesbutton"],
          };
 
          self.refresh();
+
+         $("#" + containerId + "-refresh").click(function(e) {
+            e.preventDefault();
+            self.refresh();
+         });
       }
    };
 
